2f4752e7312212a4ed5f2172e93d4cd64a44e9d7
Guilherme <guilhermebernava00@gmail.com>
2021-10-29 15:39:01 -0300

    Funcionando Aula 157

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace XadrezConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 1);
+
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+
+            Console.ReadLine();
+        }
+    }
+}
